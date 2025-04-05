1a21b527f8de70d5eeb289a58df78c6f56b1d00e
Preetha2005 <127740734+Preetha2005@users.noreply.github.com>
2025-04-05 09:47:29 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
     echo hi123
     sh 'chmod +x build.sh'
-    sh './build.sh'
+    sh '.scripts/build.sh'
     docker login -u preethasaravanan -p Preetha@2005
     docker tag test preethasaravanan/review
     docker push preethasaravanan/review
