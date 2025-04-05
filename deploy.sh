16f05cf47a775a8fd415ab9a6e57169c5b9d54cb
Preetha2005 <127740734+Preetha2005@users.noreply.github.com>
2025-04-05 09:27:55 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/ar
-    docker push ar8888/ar
+    docker login -u preethasaravanan -p Preetha@2005
+    docker tag test preethasaravanan/review
+    docker push preethasaravanan/review
     
